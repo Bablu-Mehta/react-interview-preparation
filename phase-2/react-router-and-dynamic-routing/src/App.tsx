7eb59ce05436469f1b1fffa68aa4c8446090b8b7
Bablu Mehta <babloomehta895012@gmail.com>
2025-02-04 22:23:25 +0530

    added lazy loding and suspense for more optimization.

--- phase-2/react-router-and-dynamic-routing/src/App.tsx
+++ phase-2/react-router-and-dynamic-routing/src/App.tsx
@@ -1,41 +1,44 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
-import About from "./components/About";
-import User from "./components/User";
-import NotFound from "./components/NotFound";
-import NavBar from "./components/NavBar";
-import Contact from "./components/Contact";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Dashboard from "./components/Dashboard";
-import Profile from "./components/Profile";
-import Setting from "./components/Setting";
-import Login from "./components/Login";
+import { lazy, Suspense } from "react";
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
+const User = lazy(() => import("./components/User"));
+const NotFound = lazy(() => import("./components/NotFound"));
+const NavBar = lazy(() => import("./components/NavBar"));
+const Contact = lazy(() => import("./components/Contact"));
+const ProtectedRoute = lazy(() => import("./components/ProtectedRoute"));
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Profile = lazy(() => import("./components/Profile"));
+const Setting = lazy(() => import("./components/Setting"));
+const Login = lazy(() => import("./components/Login"));
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/dashboard/*"
-          element={
-            <ProtectedRoute isAuth={false}>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Profile />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="setting" element={<Setting />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/dashboard/*"
+            element={
+              <ProtectedRoute isAuth={false}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Profile />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="setting" element={<Setting />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
